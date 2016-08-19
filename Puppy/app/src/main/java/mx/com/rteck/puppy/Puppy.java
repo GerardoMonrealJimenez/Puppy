30b98e0b5ee36eefb6862299f9c3b319c0f1e221
GerardoMonrealJimenez <gerardo.monreal.imt@gmail.com>
2016-08-19 02:48:32 -0500

    Version Final

---
+++ Puppy/app/src/main/java/mx/com/rteck/puppy/Puppy.java
@@ -0,0 +1,13 @@
+package mx.com.rteck.puppy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Puppy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puppy);
+    }
+}
